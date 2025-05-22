e8be95c72c0dda272c4a60a737a35547b80820e6
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-05-22 16:59:16 +0900

    init system

---
+++ src/app/components/LeftMenu.tsx
@@ -0,0 +1,12 @@
+export default function LeftMenu() {
+    return (
+        <div>
+            <h2 className="text-xl font-bold">Left Menu</h2>
+            <ul className="list-disc pl-5">
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </div>
+    );
+}
---
+++ src/app/components/RightMenu.tsx
@@ -0,0 +1,23 @@
+export default function RightMenu() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="/right-menu/item1" className="hover:text-gray-400">
+                        Right Menu Item 1
+                    </a>
+                </li>
+                <li>
+                    <a href="/right-menu/item2" className="hover:text-gray-400">
+                        Right Menu Item 2
+                    </a>
+                </li>
+                <li>
+                    <a href="/right-menu/item3" className="hover:text-gray-400">
+                        Right Menu Item 3
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
