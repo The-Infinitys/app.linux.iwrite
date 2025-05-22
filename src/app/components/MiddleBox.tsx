58fb205b3434cf4616b3f29546af9a074cf8b480
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-05-22 17:00:45 +0900

    fixed

--- src/app/components/MiddleBox.tsx
+++ src/app/components/MiddleBox.tsx
@@ -1,4 +1,10 @@
-export default function MiddleBox({ children }) {
+import React from "react";
+
+export default function MiddleBox({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
       <div className="flex flex-row items-start justify-between w-full max-w-6xl p-4 bg-white shadow-md rounded-lg">
