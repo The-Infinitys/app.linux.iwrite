9d5611ea8539129a4288258b3c702c7be3d72341
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-05-22 21:11:49 +0900

    checking...

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,4 +1,8 @@
 import styles from "./components.module.css";
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>made by The Infinity's</p>
+    </footer>
+  );
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,4 +1,13 @@
 import styles from "./components.module.css";
+import Image from "next/image";
+import iWriteLogo from "@/../public/logo.svg";
+import windowLogo from "@/../public/window.svg";
 export default function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <Image src={iWriteLogo} width={16} height={16} alt="" />
+      <h1 className={styles.title}>iWrite</h1>
+      <Image src={windowLogo} width={16} height={16} alt="" />
+    </header>
+  );
 }
